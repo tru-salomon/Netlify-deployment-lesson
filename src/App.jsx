2a3b058a3c2d30f28869b815911a441a28520e66
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-07-26 12:30:55 -0400

    Env Var -Buggin

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import.meta.env.VITE_DAN_VAR
+
+console.log('Dan is '`${meta.env.VITE_DAN_VAR}`)
 
 function App() {
   const [count, setCount] = useState(0)
-
+    
   return (
+    
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
